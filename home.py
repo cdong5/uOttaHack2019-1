4e34295920d0780a269daedfacf4c9669bfc3a54
Boris Nz <borisnz101@gmail.com>
2019-02-10 02:29:27 -0500

    changes

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from flask import*
+from flask import *
 import pyrebase
 
 app = Flask(__name__)
@@ -10,21 +10,38 @@ config = {
     "projectId": "uottahack-7cb21",
     "storageBucket": "uottahack-7cb21.appspot.com",
     "messagingSenderId": "418633867762"
-  };
+};
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
-@app.route('/', methods=['GET', 'POST'])
 
-def login():
-    if request.method =='POST':
+@app.route('/', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
         email = request.form['email']
         password = request.form['pass']
 
-        auth.sign_in_with_email_and_password(email, password)
+        user = auth.create_user_with_email_and_password(email, password)
+        auth.send_email_verification(user['idToken'])
 
     return render_template('classify.html')
+
+
+def login():
+    if request.method == 'post':
+        unsuccessful = 'The credentials are invalid'
+        email_log = request.form['email']
+        password_log = request.form['pass']
+
+        try:
+            auth.sign_in_with_email_and_password(email_log, password_log)
+            return "fuck"
+        except:
+            return render_template('classify.html', us=unsuccessful)
+
+    return render_template('classify.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
